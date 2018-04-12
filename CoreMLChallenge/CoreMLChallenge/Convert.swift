131f837b8699bd95b407f201543c2e26f8682795
Renan Trapp <trapprenan@gmail.com>
2018-04-12 15:38:00 -0300

    It Works

---
+++ CoreMLChallenge/CoreMLChallenge/Convert.swift
@@ -0,0 +1,42 @@
+//
+//  UIImageExtension.swift
+//  CoreMLChallenge
+//
+//  Created by Renan Trapp on 12/04/18.
+//  Copyright © 2018 Renan Trapp. All rights reserved.
+//
+
+import UIKit
+
+
+class Convert {
+   
+   @discardableResult
+   static func convertTOCVPixelBuffer(image: UIImage) -> (CVPixelBuffer, UIImage) {
+        UIGraphicsBeginImageContextWithOptions(CGSize(width: 299, height: 299), true, 1)
+        image.draw(in: CGRect(x: 0, y: 0, width: 299, height: 299))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue, kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
+        var pixelBuffer: CVPixelBuffer?
+        
+        let status = CVPixelBufferCreate(kCFAllocatorDefault, Int(newImage.size.width), Int(newImage.size.height), kCVPixelFormatType_32ARGB, attrs, &pixelBuffer)
+    
+        CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer!)
+        
+        let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+        let context = CGContext(data: pixelData, width: Int(newImage.size.width), height: Int(newImage.size.height), bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!), space: rgbColorSpace, bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue)
+        
+        context?.translateBy(x: 0, y: newImage.size.height)
+        context?.scaleBy(x: 1.0, y: -1.0)
+        
+        UIGraphicsPushContext(context!)
+        newImage.draw(in: CGRect(x: 0, y: 0, width: newImage.size.width, height: newImage.size.height))
+        UIGraphicsPopContext()
+        CVPixelBufferUnlockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        
+        return (pixelBuffer!, newImage)
+    }
+    
+}
--- CoreMLChallenge/CoreMLChallenge/ViewController.swift
+++ CoreMLChallenge/CoreMLChallenge/ViewController.swift
@@ -7,19 +7,54 @@
 //
 
 import UIKit
+import CoreML
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    let food101 = Food101()
+    
+    @IBOutlet weak var FoodNme: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var imagePicker = UIImagePickerController()
+    
+    func caption(image: CVPixelBuffer) throws -> (String, [String: Double]) {
+        //print(image)
+        let prediction = try self.food101.prediction(image: image)
+      //  print(prediction.classLabel)
+        return (prediction.classLabel, prediction.foodConfidence)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func takePicture(_ sender: Any) {
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
     }
-
-
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        self.dismiss(animated: true, completion: nil)
+        var image = UIImage()
+        var predictionDescrition = ""
+        if let pickerImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            image = pickerImage
+        }
+        
+        let convertImage = Convert.convertTOCVPixelBuffer(image: image)
+        imageView.image = convertImage.1
+        
+        guard let prediction = try? caption(image: convertImage.0) else {
+            return
+        }
+        
+        for i in prediction.1 {
+            if i.key.contains(prediction.0) {
+                print(i)
+                predictionDescrition = "This image has \(Double(floor(i.value * 100)))% of chance to be \(i.key)"
+            }
+        }
+        print(predictionDescrition)
+        self.FoodNme.text = "\(predictionDescrition)"
+        
+    }
 }
 
